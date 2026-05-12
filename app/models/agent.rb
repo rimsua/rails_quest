8e2966abc70f5a1b04da384c45e7247f8f52a24a
rimsua <zukk22708@gmail.com>
2026-05-12 09:16:12 +0000

    add associations

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,2 +1,5 @@
 class Agent < ApplicationRecord
+  has_many :missions
+  has_many :agent_skills
+  has_many :skills, through: :agent_skills
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,4 @@
 class Skill < ApplicationRecord
+  has_many :agent_skills
+  has_many :agents, through: :agent_skills
 end
